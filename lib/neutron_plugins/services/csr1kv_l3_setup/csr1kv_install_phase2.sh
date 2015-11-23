5e2e2f7846265c8dad33fafb4917d0f5552b846d
Bob Melander <bob.melander@gmail.com>
2015-11-23 18:12:30 +0100

    Make parts of CSR1kv setup scripts optional
    This patch makes some changes to simplify running
    mulitple config agents using Devstack.
    
    Change-Id: I5c05fd1aa7011e2575e8b480733b2ed8d0f97fa0

--- lib/neutron_plugins/services/csr1kv_l3_setup/csr1kv_install_phase2.sh
+++ lib/neutron_plugins/services/csr1kv_l3_setup/csr1kv_install_phase2.sh
@@ -37,25 +37,30 @@ if [[ ! -z $localrc && -f $localrc ]]; then
     eval $(grep ^Q_CISCO_CONFIGURE_NOVA_AND_GLANCE= $localrc)
 fi
 CREATE_TEST_NETWORKS=$(trueorfalse "False" $Q_CISCO_CREATE_TEST_NETWORKS)
-CONFIGURE_NOVA_AND_GLANCE=$(trueorfalse "True" $Q_CISCO_CREATE_TEST_NETWORKS)
+CONFIGURE_NOVA_AND_GLANCE=$(trueorfalse "True" $CONFIGURE_NOVA_AND_GLANCE)
+Q_CISCO_IS_EXTRA_CONFIG_AGENT=$(trueorfalse "True" $Q_CISCO_IS_EXTRA_CONFIG_AGENT)
 
 source ~/devstack/openrc $osn L3AdminTenant
 
-if  [[ "$CONFIGURE_NOVA_AND_GLANCE" == "True"  ]]; then
-    echo "***************** Setting up Nova & Glance for CSR1kv *****************"
-    ./setup_nova_and_glance_for_csr1kv_l3.sh $osn $localrc $mysql_user $mysql_password
-fi
+if [[ "$Q_CISCO_IS_EXTRA_CONFIG_AGENT" == "False" ]]; then
+    if  [[ "$CONFIGURE_NOVA_AND_GLANCE" == "True" ]]; then
+        echo "***************** Setting up Nova & Glance for CSR1kv *****************"
+        ./setup_nova_and_glance_for_csr1kv_l3.sh $osn $localrc $mysql_user $mysql_password
+    fi
 
-echo "***************** Setting up Neutron for CSR1kv *****************"
-./setup_neutron_for_csr1kv_l3.sh $osn $plugin $localrc
+    echo "***************** Setting up Neutron for CSR1kv *****************"
+    ./setup_neutron_for_csr1kv_l3.sh $osn $plugin $localrc
+fi
 
 echo "***************** Setting up CfgAgent connectivity *****************"
 ./setup_l3cfgagent_networking.sh $osn $plugin $localrc $mgmt_ip
 
-if [[ "$CREATE_TEST_NETWORKS" == "True" ]]; then
-    source ~/devstack/openrc admin demo
-    echo "***************** Setting up test networks *****************"
-   ./setup_test_networks.sh $osn $plugin
-   ./setup_interface_on_extnet1_for_demo.sh $osn $plugin
+if [[ "$Q_CISCO_IS_EXTRA_CONFIG_AGENT" == "False" ]]; then
+    if [[ "$CREATE_TEST_NETWORKS" == "True" ]]; then
+        source ~/devstack/openrc admin demo
+        echo "***************** Setting up test networks *****************"
+       ./setup_test_networks.sh $osn $plugin
+       ./setup_interface_on_extnet1_for_demo.sh $osn $plugin
+    fi
 fi
 echo 'Done!...'
